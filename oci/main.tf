7fff6d226bf847e664107b20aa4d70230c540170
Simon Dobson <simon.dobson@computer.org>
2023-02-24 16:19:58 +0000

    Added copyright notices

--- oci/main.tf
+++ oci/main.tf
@@ -1,3 +1,22 @@
+# Terraform script to spin-up a compute cluster on OCI
+#
+# Copyright (C) 2023 Simon Dobson
+#
+# This file is part of cloud-epydemic, network simulation as a service
+#
+# cloud-epydemic is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published byf
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# cloud-epydemic is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with cloud-epydemic. If not, see <http://www.gnu.org/licenses/gpl.html>.
+
 # ---------- OCI provider ----------
 
 terraform {
