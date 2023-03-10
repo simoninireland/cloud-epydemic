2dd149c234da0ce1b7dc705c57ac0ed6b54c3849
Simon Dobson <simon.dobson@computer.org>
2023-03-10 16:07:21 +0000

    Work in progress

---
+++ digitalocean/main.tf
@@ -0,0 +1,47 @@
+# Containerised compute cluster on Digital Ocean
+#
+# Copyright (C) 2023 Simon Dobson
+#
+# This file is part of cloud-epydemic, network simulation as a service
+#
+# cloud-epydemic is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published byf
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# cloud-epydemic is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with cloud-epydemic. If not, see <http://www.gnu.org/licenses/gpl.html>.
+
+# ---------- Droplet ----------
+
+resource "digitalocean_droplet" "cluster" {
+  image = "ubuntu-20-04-x64"
+  name = "cluster"
+  region = "lon1"
+  size = "s-1vcpu-1gb"
+  ssh_keys = [
+    data.digitalocean_ssh_key.cloudepydemic.id
+  ]
+
+  connection {
+    host = self.ipv4_address
+    user = "root"
+    type = "ssh"
+    private_key = file(var.private_key_file)
+    timeout = "2m"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "export PATH=$PATH:/usr/bin",
+      # install nginx
+      "sudo apt update",
+      "sudo apt install -y nginx"
+    ]
+  }
+}
--- oci/main.tf
+++ oci/main.tf
@@ -17,39 +17,12 @@
 # You should have received a copy of the GNU General Public License
 # along with cloud-epydemic. If not, see <http://www.gnu.org/licenses/gpl.html>.
 
-# ---------- Storage ----------
-
-resource "oci_file_storage_file_system" "cluster_shared" {
-  availability_domain      = data.oci_identity_availability_domains.cluster_availability.availability_domains.2.name
-  compartment_id           = var.compartment_ocid
-  display_name             = "Cluster shared working storage"
-}
-
-resource "oci_file_storage_mount_target" "cluster_shared_mount" {
-  availability_domain      = data.oci_identity_availability_domains.cluster_availability.availability_domains.2.name
-  compartment_id           = var.compartment_ocid
-  subnet_id = oci_core_subnet.cluster_subnet.id
-  display_name = "Cluster shared working storage mount point"
-}
-
-resource "oci_file_storage_export_set" "cluster_shared_export_set" {
-  mount_target_id = oci_file_storage_mount_target.cluster_shared_mount.id
-  display_name = "Cluster shared storage export set"
-}
-
-resource "oci_file_storage_export" "cluster_shared_export" {
-  export_set_id =  oci_file_storage_export_set.cluster_shared_export_set.id
-  file_system_id = oci_file_storage_file_system.cluster_shared.id
-  path = "/"
-}
-
-
 # ---------- Containers ----------
 
-resource "oci_container_instances_container_instance" "cluster_frontend_instance" {
+resource "oci_container_instances_container_instance" "cluster_authentication_instance" {
   availability_domain      = data.oci_identity_availability_domains.cluster_availability.availability_domains.2.name
   compartment_id           = var.compartment_ocid
-  display_name             = "Cluster frontend"
+  display_name             = "Cluster authentication server"
 
   shape                    = "CI.Standard.E3.Flex"
   shape_config {
@@ -58,22 +31,12 @@ resource "oci_container_instances_container_instance" "cluster_frontend_instance
   }
   vnics {
     subnet_id             = oci_core_subnet.cluster_subnet.id
-    is_public_ip_assigned = true
-  }
-  volumes {
-    name = "shared"
-    volume_type = "EMPTYDIR"
   }
 
   container_restart_policy = "ALWAYS"
   containers {
-    image_url    = "simonireland/base-notebook:latest"
-    display_name = "Cluster notebook frontend"
-
-    volume_mounts {
-      volume_name = "shared"
-      mount_path = "/home/epydemic/shared"
-    }
+    image_url    = "redis:latest"
+    display_name = "Cluster authentication"
   }
 }
 
@@ -98,12 +61,9 @@ resource "oci_container_instances_container_instance" "cluster_frontend_instance
 #     image_url    = "simonireland/controller:latest"
 #     display_name = "Cluster controller"
 
-#     volume_mounts {
-#       volume_name = "cluster_shared"
-#       mount_path = "/home/epydemic/shared"
-#     }
 #     environment_variables = {
 #       EPYDEMIC_CONTROLLER_HOST = "cluster_controller"
+#       EPYDEMIC_AUTHENTICATION_HOST = "cluster_authentication"
 #     }
 #   }
 # }
@@ -134,6 +94,37 @@ resource "oci_container_instances_container_instance" "cluster_frontend_instance
 #     }
 #     environment_variables = {
 #       EPYDEMIC_ENGINES = 4
+#     }
+#   }
+# }
+
+# resource "oci_container_instances_container_instance" "cluster_frontend_instance" {
+#   availability_domain      = data.oci_identity_availability_domains.cluster_availability.availability_domains.2.name
+#   compartment_id           = var.compartment_ocid
+#   display_name             = "Cluster frontend"
+
+#   shape                    = "CI.Standard.E3.Flex"
+#   shape_config {
+#     memory_in_gbs = 4
+#     ocpus         = 1
+#   }
+#   vnics {
+#     subnet_id             = oci_core_subnet.cluster_subnet.id
+#     is_public_ip_assigned = true
+#   }
+#   volumes {
+#     name = "data"
+#     volume_type = "EMPTYDIR"
+#   }
+
+#   container_restart_policy = "ALWAYS"
+#   containers {
+#     image_url    = "simonireland/base-notebook:latest"
+#     display_name = "Cluster notebook frontend"
+
+#     volume_mounts {
+#       volume_name = "data"
+#       mount_path = "/home/epydemic/data"
 #     }
 #   }
 # }
