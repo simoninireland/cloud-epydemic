04d7cd4b5558c709c995fcaa822f4bebb7dae7bc
Simon Dobson <simon.dobson@computer.org>
2023-03-15 17:42:44 +0000

    Removed unnecessary json package import

--- containers/micro-engine/micro_engine/engine.py
+++ containers/micro-engine/micro_engine/engine.py
@@ -17,7 +17,6 @@
 # You should have received a copy of the GNU General Public License
 # along with cloud-epydemic. If not, see <http://www.gnu.org/licenses/gpl.html>.
 
-import json
 import base64
 import pickle
 
