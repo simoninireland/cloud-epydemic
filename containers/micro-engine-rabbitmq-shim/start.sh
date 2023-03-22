778a86fe94873e6796002bb5b43479773b38ba48
Simon Dobson <simon.dobson@computer.org>
2023-03-22 17:20:31 +0000

    Refactored start-up (there might be a race?)

---
+++ containers/micro-engine-rabbitmq-shim/start.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# Start-up root script for engien microservice/message broker shim
+#
+# Copyright (C) 2023 Simon Dobson
+#
+# This file is part of cloud-epydemic, network simulation as a service
+#
+# cloud-epydemic is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published byf
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# cloud-epydemic is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with cloud-epydemic. If not, see <http://www.gnu.org/licenses/gpl.html>.
+
+sleep 5
+
+# Run the shim script
+exec python ./start_shim.py
