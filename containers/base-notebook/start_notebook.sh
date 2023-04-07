8132ac129c6cdb643e35dae99feab539bee3b4ec
Simon Dobson <simon.dobson@computer.org>
2023-04-07 08:32:00 +0100

    Initial frontend

--- containers/base-notebook/start_notebook.sh
+++ containers/base-notebook/start_notebook.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Start-up script for clkuster frontend notebook server
+# Start-up script for cluster frontend notebook
 #
 # Copyright (C) 2023 Simon Dobson
 #
@@ -19,34 +19,19 @@
 # You should have received a copy of the GNU General Public License
 # along with cloud-epydemic. If not, see <http://www.gnu.org/licenses/gpl.html>.
 
-# We expect three environment variables to be defined by the container:
+# We expect tweo environment variables to be defined by the container:
 #
 # - EPYDEMIC_HOME -- the home directory
-# - EPYDEMIC_DATA -- the directory containing the notebooks and data
-# - EPYDEMIC_PROFILE -- the IPython profile for the cluster
+# - EPYDEMIC_DATA -- the notebook data directory
 #
 # The data directory will typically be a mount point, to allow notebooks
 # and data to persist.
 #
-# We also accept one required and one optional run-time variable:
+# We also accept one optional run-time variable:
 #
-# - EPYDEMIC_AUTHENTICATION_HOST -- the name or IP address of the machine
-#                                   running the Redis server used for
-#                                   secret sharing
 # - EPYDEMIC_PASSWORD -- the password for accessing the notebook server
 #
 # The password is blank by default.
-
-# Create the profile
-ipython profile create --parallel $EPYDEMIC_PROFILE
-EPYDEMIC_PROFILE_DIR=`ipython profile locate $EPYDEMIC_PROFILE`
-
-# Retrieve the access tokens
-echo "`./kv $EPYDEMIC_PROFILE/ssh/id_rsa`" >.ssh/id_rsa
-chmod go-rwx .ssh/id_rsa
-echo "`./kv $EPYDEMIC_PROFILE/ssh/controller_fingerprint`" >.ssh/known_hosts
-EPYDEMIC_CLIENT_JSON=$EPYDEMIC_PROFILE_DIR/security/ipcontroller-client.json
-echo "`./kv $EPYDEMIC_PROFILE/ipyparallel/client_json`" >$EPYDEMIC_CLIENT_JSON
 
 # Grab password from the environment, if present
 PASSWORD=${EPYDEMIC_PASSWORD:-}
